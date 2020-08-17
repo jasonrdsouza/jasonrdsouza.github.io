a4a123b699fbbda891574e92282294e95707eacc
Jason Dsouza <jasonrdsouza@gmail.com>
2020-08-16 21:29:39 -0400

    Remove unneeded packages symlink so Github Pages works

--- build_release.sh
+++ build_release.sh
@@ -7,6 +7,7 @@ mkdir docs
 echo "Generating new site"
 pub run build_runner build --release --output release
 cp -R release/web/ docs/
+rm docs/packages
 rm -rf release
 
 echo "Finished generating site into docs/"
