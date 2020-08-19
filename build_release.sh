91339ad974bc0f9a47c1fe6544b5a05642f24ab7
Jason Dsouza <jasonrdsouza@gmail.com>
2020-08-18 21:33:09 -0400

    Fix site generation to preserve github generated files
    CNAME and nojekyll files were being cleared out with new site
    generation.

--- build_release.sh
+++ build_release.sh
@@ -7,6 +7,8 @@ mkdir docs
 echo "Generating new site"
 pub run build_runner build --release --output release
 cp -R release/web/ docs/
+cp CNAME docs/CNAME
+touch docs/.nojekyll
 rm docs/packages
 rm -rf release
 
