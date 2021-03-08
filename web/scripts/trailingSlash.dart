835963bcd3276c78ad456e7accb2a7cfd19edc2c
Jason Dsouza <jasonrdsouza@gmail.com>
2021-03-07 21:24:54 -0500

    Todo: fix infinite redirect
    For now, switch back to working behavior

--- web/scripts/trailingSlash.dart
+++ web/scripts/trailingSlash.dart
@@ -7,6 +7,11 @@ import 'dart:html';
 // Google has indexed a bunch of posts with the trailing slash, so at the very least this will ensure
 // that those links don't break, but I'm sure there are other trailing slash links scattered around
 // the web.
+//
+// Additionally, in cases where the page doesn't exist, Github Pages retries with an added trailing
+// slash (if one doesn't exist). Since I remove the trailing slash, and they add it, this results
+// in an endless redirect loop when we hit the 404 page. Need to figure out how to deal with that...
+
 void main() {
   var currentLocation = window.location.href;
   if (currentLocation.endsWith("/")) {
